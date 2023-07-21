d82c8b4d9e865045b523af6244880ac7a7315f2b
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-07-21 14:30:44 +0700

    build: create src folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar.jsx';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div>This is Home page</div>
+    </>
+  );
+}
