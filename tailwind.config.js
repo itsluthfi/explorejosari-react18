4e6a9fd541fa027f9101db211cd5a42133b7f766
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-07-24 12:51:40 +0700

    build: config tailwindcss font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto-sans': ['Noto Sans', 'sans-serif'],
+      },
+
+      colors: {
+        hijau: '#709A08',
+        'hijau-gelap': '#416100',
+        merah: '#FF0000',
+      },
+    },
   },
   plugins: [],
 };
