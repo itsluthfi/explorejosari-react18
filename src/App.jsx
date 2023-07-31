d273a55ab4fe015f0c50e71e3969f6bb5b655f8e
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-07-31 14:49:16 +0700

    feat: create UMKM page and register page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import UMKM from './pages/UMKM.jsx';
 import './index.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/umkm" element={<UMKM />} />
     </Routes>
   );
 }
