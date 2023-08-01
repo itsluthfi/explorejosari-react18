796f2993d7ae2ea462b533bd9ceae6c1a08896f0
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-08-01 16:08:38 +0700

    feat: create under construction page and regsiter page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import UMKM from './pages/UMKM.jsx';
+import UnderConstruction from './pages/UnderConstruction.jsx';
 import './index.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/umkm" element={<UMKM />} />
+      <Route path="/*" element={<UnderConstruction />} />
     </Routes>
   );
 }
